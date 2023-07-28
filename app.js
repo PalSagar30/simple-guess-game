614c737acfecf48763f9a09e417964066003891f
PalSagar30 <sagarpalsp3008@gmail.com>
2023-07-28 22:05:22 +0530

    add simple logic

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 const max = prompt("enter maximum number");
+
+const random = Math.floor(Math.random()*max)+1;
+
+let guess = prompt("guess the number");
+
+// console.log(random)
+
+while(true){
+    if(guess == "quit"){
+        console.log("user quit")
+        break;
+    }
+    if(guess == random){
+        console.log("woooooooooooooooooooooooo")
+        break;
+    }else{
+        guess = prompt("your guess is wrong! please try again")
+    }
+}
